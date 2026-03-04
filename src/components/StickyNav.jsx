ec201d438a94c4d3ceb595964bc95355d3a6142b
Delta Block <deltablockv@gmail.com>
2026-03-04 21:01:34 +0000

    File changes

--- src/components/StickyNav.jsx
+++ src/components/StickyNav.jsx
@@ -32,7 +32,7 @@ export default function StickyNav({ currentPage = 'home' }) {
             src={LOGO_URL}
             alt="Operator.ink"
             className="h-auto object-contain"
-            style={{ width: '240px', maxWidth: '55vw' }}
+            style={{ width: '240px', maxWidth: '40vw' }}
           />
         </a>
         <div className="flex items-center gap-2 sm:gap-4 flex-shrink-0">
